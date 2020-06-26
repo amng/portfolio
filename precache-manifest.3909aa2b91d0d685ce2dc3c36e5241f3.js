185b50b6de5bfc7e77bc3e33962ad5d4ff6d7cee
Artur Gomes <arturngomes@gmail.com>
2020-06-26 16:19:42 +0100

    First commit, added portfolio for testing

---
+++ precache-manifest.3909aa2b91d0d685ce2dc3c36e5241f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf5e42a5feba8d61535842fa1b4c6d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0766e3659ee80d121d5f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c5ef43267bd9bf85852",
+    "url": "/static/js/2.facdff10.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.facdff10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0766e3659ee80d121d5f",
+    "url": "/static/js/main.9f017745.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
