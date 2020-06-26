2e6f22f412603b287876b85d0469d8161c4ac971
Artur Gomes <arturngomes@gmail.com>
2020-06-26 17:00:14 +0100

    Fixed urls for github

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3909aa2b91d0d685ce2dc3c36e5241f3.js"
+  "https://raw.githubusercontent.com/amng/portfolio/master/precache-manifest.053bdec77b93bd44040489ef3a4ce763.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://raw.githubusercontent.com/amng/portfolio/master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
