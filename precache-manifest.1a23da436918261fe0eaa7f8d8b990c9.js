ab17d5c9085a4e48d5c963700b3a9e00d6b366b5
Artur Gomes <arturngomes@gmail.com>
2023-05-15 22:58:16 +0100

    Update 2023 May

---
+++ precache-manifest.1a23da436918261fe0eaa7f8d8b990c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a10ab56593ac214cf0d59826862ce6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e27953059aca90363248",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64e66493549544b081fe",
+    "url": "./static/js/2.4df86a42.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "./static/js/2.4df86a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27953059aca90363248",
+    "url": "./static/js/main.41189fdc.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://amng.github.io/portfolio/precache-manifest.5e7b721ae96dea5f860ac158d2149871.js"
+  "./precache-manifest.1a23da436918261fe0eaa7f8d8b990c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://amng.github.io/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
